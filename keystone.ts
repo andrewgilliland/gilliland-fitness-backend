59307273711fc06525cd0a72ec79924c8cae16bb
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-04 14:41:03 -0500

    adds mail.ts

--- keystone.ts
+++ keystone.ts
@@ -9,6 +9,7 @@ import { User } from "./schemas/User";
 import { Product } from "./schemas/Product";
 import { ProductImage } from "./schemas/ProductImage";
 import { insertSeedData } from "./seed-data";
+import { sendPasswordResetEmail } from "./lib/mail";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/gilliland-fitness";
@@ -25,6 +26,12 @@ const { withAuth } = createAuth({
   initFirstItem: {
     fields: ["name", "email", "password"],
     // TODO: Add in initial roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
   },
 });
 
