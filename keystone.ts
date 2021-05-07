8f8d4bc82c6784fcc7dd67d99266866abf7a0c02
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-07 09:34:17 -0500

    adds addtocart

--- keystone.ts
+++ keystone.ts
@@ -11,6 +11,7 @@ import { ProductImage } from "./schemas/ProductImage";
 import { CartItem } from "./schemas/CartItem";
 import { insertSeedData } from "./seed-data";
 import { sendPasswordResetEmail } from "./lib/mail";
+import { extendGraphqlSchema } from "./mutations";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/gilliland-fitness";
@@ -61,6 +62,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       // Show the UI only for people who pass this test
       isAccessAllowed: ({ session }) => !!session?.data,
