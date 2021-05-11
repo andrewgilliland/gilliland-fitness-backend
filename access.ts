a88669af817b7d342fc4bb63a085485922e1d26d
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-11 13:57:13 -0500

    updates user schema

--- access.ts
+++ access.ts
@@ -70,4 +70,16 @@ export const rules = {
     // They should only see available products (based on the status field)
     return { status: "AVAILABLE" };
   },
+
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+    // Otherwise they may only update themselves
+    return { id: session.itemId };
+  },
 };
--- schemas/User.ts
+++ schemas/User.ts
@@ -1,10 +1,20 @@
 import { list } from "@keystone-next/keystone/schema";
 import { text, password, relationship } from "@keystone-next/fields";
+import { permissions, rules } from "../access";
 
 export const User = list({
-  // access:
-
-  // ui
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    // only people with the permission can delete themselves!
+    // You can't delete yourself
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
@@ -20,6 +30,10 @@ export const User = list({
     orders: relationship({ ref: "Order.user", many: true }),
     role: relationship({
       ref: "Role.assignedTo",
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
     }),
     products: relationship({
       ref: "Product.user",
