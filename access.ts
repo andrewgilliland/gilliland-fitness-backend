3eb9aca9763b8f8936f4bbbdbde3419a84defbd7
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-11 11:56:18 -0500

    updates access.ts

--- access.ts
+++ access.ts
@@ -1,6 +1,23 @@
+import { permissionsList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 // At it's simplest, access control returns a yes or no value depending on the users session
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissions check if someone meets a criteria - yes or no
+export const permissions = {
+  ...generatedPermissions,
+};
+
+// Rule based function
