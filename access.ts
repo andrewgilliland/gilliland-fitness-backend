c7eb5267a17faa8a61dff4532f5cd4cd466cd215
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-11 13:42:39 -0500

    adds cart and order based rules

--- access.ts
+++ access.ts
@@ -24,6 +24,9 @@ export const permissions = {
 // Rules can return a boolean - yes or no - or a filter which limits which products they can CRUD
 export const rules = {
   canManageProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
     // 1. Do they have the permission of canManageProducts
     if (permissions.canManageProducts({ session })) {
       return true;
@@ -32,7 +35,34 @@ export const rules = {
     return { user: { id: session.itemId } };
   },
 
+  canOrder({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    return { user: { id: session.itemId } };
+  },
+
+  canManageOrderItems({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    return { order: { user: { id: session.itemId } } };
+  },
+
   canReadProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
     if (permissions.canManageProducts({ session })) {
       return true; // They can read everything!
     }
--- schemas/OrderItem.ts
+++ schemas/OrderItem.ts
@@ -1,8 +1,14 @@
 import { integer, relationship, select, text } from "@keystone-next/fields";
 import { list } from "@keystone-next/keystone/schema";
+import { isSignedIn, rules } from "../access";
 
 export const OrderItem = list({
-  // access:
+  access: {
+    create: isSignedIn,
+    read: rules.canManageOrderItems,
+    update: () => false,
+    delete: () => false,
+  },
   fields: {
     name: text({
       isRequired: true,
